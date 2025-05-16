2eb06399fef4d0aecc0b0d97fc1a9f6c5bb86780
namirahrasul <n.rsl.136@gmail.com>
2025-05-16 22:19:47 +0600

    replaced with m2s unet

--- models/p2_weighing_old/respace.py
+++ models/p2_weighing_old/respace.py
@@ -126,6 +126,3 @@ class _WrappedModel:
         if self.rescale_timesteps:
             new_ts = new_ts.float() * (1000.0 / self.original_num_steps)
         return self.model(x, new_ts, **kwargs)
-
-
-
